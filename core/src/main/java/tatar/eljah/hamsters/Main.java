4e2776fa483f627fa6c454f33df0c5db6a4df3ba
Ilya <eljah@mail.ru>
2025-09-07 19:03:42 +0300

    Pair thin guide lines correctly

--- core/src/main/java/tatar/eljah/hamsters/Main.java
+++ core/src/main/java/tatar/eljah/hamsters/Main.java
@@ -214,13 +214,20 @@ public class Main extends ApplicationAdapter {
         }
         java.util.Collections.sort(ys);
         java.util.ArrayList<float[]> pairs = new java.util.ArrayList<>();
-        for (int i = 0; i + 1 < ys.size(); i += 2) {
-            float topFromSvg = ys.get(i) + GUIDE_STROKE_WIDTH;
-            float bottomFromSvg = ys.get(i + 1) - GUIDE_STROKE_WIDTH;
-            if (bottomFromSvg > topFromSvg) {
-                float top = 600f - bottomFromSvg;
-                float bottom = 600f - topFromSvg;
-                pairs.add(new float[]{top, bottom});
+        for (int i = 0; i + 1 < ys.size();) {
+            float y1 = ys.get(i);
+            float y2 = ys.get(i + 1);
+            if (y2 - y1 < 60f) {
+                float topFromSvg = y1 + GUIDE_STROKE_WIDTH;
+                float bottomFromSvg = y2 - GUIDE_STROKE_WIDTH;
+                if (bottomFromSvg > topFromSvg) {
+                    float top = 600f - bottomFromSvg;
+                    float bottom = 600f - topFromSvg;
+                    pairs.add(new float[]{top, bottom});
+                }
+                i += 2;
+            } else {
+                i++;
             }
         }
         return pairs.toArray(new float[0][]);
